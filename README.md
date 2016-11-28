69212df95cc5af1858518fe0b29ddf98915c632a
Daloonik <daloonik@gmail.com>
2016-11-28 14:12:32 -0500

    Updated readme and changed code

---
+++ README.md
@@ -0,0 +1,54 @@
+# BloggerBot
+## A bot that posts to blogger.com
+This bot posts to a blog hosted by Blogger with the intent that the posts will show up on Twitter. The blog uses Twitterfeed to synchronize with a Twitter account. This bot is intentionally made to Tweet random users, so **please note** that your account will eventually be banned by Twitter as it is against their terms and conditions for a bot to @ someone. 
+
+Twitter now requires adding a mobile phone to a Twitter profile before creating an application. This bot is intended to subvert this restriction for those who don't care to give their phone numbers to a Big Data company.
+
+To get this Bot to post to Twitter, you will need to:
+
+<<<<<<< HEAD
+1. Create a blog on Blogger with your Google account.
+
+2. ~~Create a Twitterfeed account~~ As of 31 October 2016, Twitterfeed is offline. Go to IFTTT, use the "Share your New Blogger Posts to Twitter" application by dude3966, and make sure only `{{Post Content}}` is selected.
+
+3. In Blogger, go to Settings -> E-mail and activate publishing by e-mail. You can also set your secret e-mail address here.
+
+4. Finally, you will need a Wordnik account. Create one and then go to http://developer.wordnik.com/ to request an API key.
+=======
+1. Create a blog on Blogger with your Google account
+
+- Create a Twitterfeed account, insert your Blogger URL and next synchronize your Twitter account. You can tell Twitterfeed to synchronize up to 5 postings at a time.
+
+- In Blogger, go to Settings -> E-mail and activate publishing by e-mail. You can also set your secret e-mail address here.
+
+- Finally, you will need a Wordnik account. Create one and then go to http://developer.wordnik.com/ to request an API key.
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+
+You will then need a list of Twitter users to randomly Tweet, and without access to the Twitter API, you will have to create a file with a list of Twitter users. An example file is in /war/WEB-INF/StaticFiles/twits.
+You can create your own list of Twitter users to message by signing in to Twitter and going to someone's followers page and then extracting the Twitter followers. Here is how to do this:
+ 
+1. Find a Twitter user that has lots of followers and go to the follower page. For example, https://twitter.com/Barackobama/followers.
+ 
+- Next you will need to scroll down until the page displays a sufficient number of followers. Hold the page down button for a wee while. Then press Ctrl+A to copy the contents of the page and paste it into a file called users.
+
+- Now remove the first 20 or so lines until you are left with a file with lines containing something like this:
+
+        Actions de l'utilisateur 
+         Suivre
+        Daloonik
+        @daloonik
+        Profile information
+    
+- The Twitter Handle is among those lines. Use the following code:
+
+         awk "/^\@/" users >> twits
+         
+to extract the handle name and create your own twits file.   
+
+<<<<<<< HEAD
+This bot has pretty basic functionality. A nice thing would be able to respond to users and add hashtags. Without access to the Twitter API it's more difficult to do this.
+
+**Please note** this bot is further limited due to the quotas placed on e-mailing external accounts (only 10 per day allowed). A workaround would be to e-mail an administrator account, and in the administrator account set up a filter which forwards to your secret blogger posting address.
+=======
+This bot has pretty basic functionality. A nice thing would be able to respond to users and add hashtags. Without access to the Twitter API it's more difficult to do this.
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
---
+++ src/bloggerbot/BloggerbotServlet.java
@@ -0,0 +1,180 @@
+/*
+<<<<<<< HEAD
+ * Bloggerbot is licenced under the The MIT License (MIT)
+=======
+ * BloggerBot is licenced under the The MIT License (MIT)
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+ * 
+ * Copyright (c) 2014–2016 Daloonik daloonik@gmail.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package bloggerbot;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+<<<<<<< HEAD
+import com.google.appengine.api.utils.SystemProperty;
+
+=======
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+import net.jeremybrooks.knicker.KnickerException;
+import net.jeremybrooks.knicker.WordApi;
+import net.jeremybrooks.knicker.WordsApi;
+import net.jeremybrooks.knicker.dto.Example;
+import net.jeremybrooks.knicker.dto.Word;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@SuppressWarnings("serial")
+public class BloggerbotServlet extends HttpServlet
+   {
+<<<<<<< HEAD
+   public static String blogemail = "your_secret_blog_email";
+   
+=======
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+      {
+      StringBuffer buffer = new StringBuffer(); 
+      Properties props = new Properties();
+      Session session = Session.getDefaultInstance(props, null);
+<<<<<<< HEAD
+      GetLine twit = new GetLine("WEB-INF/StaticFiles/twits"); 
+=======
+      GetLine twit = new GetLine("WEB-INF/StaticFiles/twits");
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+      
+      System.setProperty("WORDNIK_API_KEY", "your_wordnik_api_key");
+      
+      try
+<<<<<<< HEAD
+         { 
+=======
+         {   
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+         do 
+            {
+            buffer.setLength(0);
+            Word random = WordsApi.randomWord();
+            Example ex = WordApi.topExample(random.getWord());
+<<<<<<< HEAD
+            buffer.append(ex.getText());            
+            } while(buffer.length() > 140); 
+        
+=======
+            buffer.append(ex.getText());
+            } while(buffer.length() > 140);
+         
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+         if(buffer.length() < 120)
+            {
+            buffer.append(" #");
+            buffer.append(WordsApi.randomWord().getWord());
+            }
+         
+         if(buffer.length() < 125)
+            {
+            buffer.insert(0, twit.line() + " ");
+            }
+<<<<<<< HEAD
+      
+         Message msg = new MimeMessage(session);
+         msg.setFrom(new InternetAddress(SystemProperty.applicationId.get() + "@appspot.gserviceaccount.com"));
+         msg.addRecipient(Message.RecipientType.TO,
+                          new InternetAddress(blogemail, "Bloggerbot"));
+
+         msg.setContent(buffer.toString(),"text/html; charset=utf-8");
+         
+         Transport.send(msg);
+         
+=======
+         Message msg = new MimeMessage(session);
+         msg.setFrom(new InternetAddress("noreply@anybody.com", "Bloggerbot"));
+         msg.addRecipient(Message.RecipientType.TO,
+                          new InternetAddress("your_secret_address", "Blog"));
+         msg.setContent(buffer.toString(),"text/html; charset=utf-8");
+         Transport.send(msg);
+
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+         resp.setContentType("text/plain");
+         resp.getWriter().println("Posted: ");
+         resp.getWriter().println(buffer.toString());
+         }
+      catch(KnickerException e)
+         {
+<<<<<<< HEAD
+    	  resp.getWriter().println("Problem with Knicker!");
+          resp.setContentType("text/plain");
+          resp.getWriter().println(e.toString());
+         }
+      catch (AddressException e)
+         {
+    	 resp.getWriter().println("Problem involving wrongly formatted address!");
+=======
+         e.printStackTrace();
+         System.exit(1);
+         }
+      catch (AddressException e)
+         {
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+         resp.setContentType("text/plain");
+         resp.getWriter().println(e.toString());
+         }
+      catch (MessagingException e)
+         {
+<<<<<<< HEAD
+     	 resp.getWriter().println("Problem with Email!");
+=======
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+         resp.setContentType("text/plain");
+         resp.getWriter().println(e.toString());
+         }    
+      catch(FileNotFoundException e)
+         {
+<<<<<<< HEAD
+     	 resp.getWriter().println("File not found!");
+         resp.setContentType("text/plain");
+         resp.getWriter().println(e.toString());       
+         }
+      catch(Exception e)
+      	{
+      	 resp.getWriter().println("Problem!");
+      	 resp.setContentType("text/plain");
+      	 resp.getWriter().println(e.toString()); 
+      	}
+=======
+         resp.setContentType("text/plain");
+         resp.getWriter().println(e.toString());       
+         }
+>>>>>>> c0949e2fab9d2c90dc71d620c872239593cd2b52
+      }
+   }
